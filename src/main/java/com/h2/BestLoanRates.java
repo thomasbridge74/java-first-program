398b37bd5225473cf09b687ccc19632541979c7b
Thomas Bridge <thomas.bridge@icloud.com>
2022-02-10 17:23:49 +0000

    Up to Step 5

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,13 @@
+package com.h2;
+
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static void main ( String[] args ) {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        name = scanner.nextLine();
+        System.out.println("Hello" + name);
+    }
+}
